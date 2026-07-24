7c577979f2811801e983a1d9c92afa054b5a77d7
GeorgePeroudis <giorgosp10042@gmail.com>
2026-07-25 00:39:17 +0300

    Refactor app structure and components
    - Removed the old explore and index screens, replacing them with a new tab layout.
    - Introduced new screens: Add Meal, All Meals, and Home, with corresponding components.
    - Created a global styles file for consistent styling across the app.
    - Implemented MacroCard and MacroGrid components for displaying nutritional information.
    - Updated navigation to use expo-router for better routing management.
    - Removed unused components and styles to streamline the codebase.

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import HomeHeader from "@/components/HomeHeader";
+import MacroGrid from "@/components/MacroGrid";
+import { globalStyles } from "@/styles/global";
+import { ScrollView, Text } from "react-native";
+
+export default function HomeScreen() {
+    return (
+        <ScrollView style={globalStyles.container}>
+            <Text style={globalStyles.title}>MacroZone</Text>
+            <HomeHeader />
+            <MacroGrid />
+        </ScrollView>
+    );
+}
